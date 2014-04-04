5621ffbd75cfe252c66aa69cfc00c737dd868036
zachprinz <zprinzbach@gmail.com>
2014-04-04 16:13:45 -0400

    Update Version

--- Board.js
+++ Board.js
@@ -1,7 +1,4 @@
 /**
- * Created by Zachary on 4/4/2014.
- */
-/**
  * Created by Zachary on 3/1/14.
  */
 
@@ -10,40 +7,74 @@ Board = function(game){
     this.frameTime = 0;
     this.previousTime = 0;
     this.hooks = [];
+	this.leftWall = null;
+	this.rightWall = null;
     this.numberOfHooks = 0;
-    this.userPosition = 0;
+	this.userPosition = 0;
+	this.userPositionY = 0;
+	this.boardScale = 1.4;
+	this.userScore = 0;
 }
 
 Board.prototype = {
     preload: function(){
         game.load.image('background','images/board.png');
         game.load.image('blank','images/blank.png');
-        this.game.load.image('hook','images/hook.png');
+    	this.game.load.image('hook','images/hook.png');
+		this.game.load.image('wall','images/wall.png');
+    	game.load.image('ground','images/ground.png');
+		game.load.image('score','images/score.png');
     },
     create: function(){
         this.sprite = game.add.sprite(0,0,'background');
+		this.leftWall = new Wall(this.game,this);
+		this.rightWall = new Wall(this.game,this);
+		this.leftWall.create(-300,0);
+		this.rightWall.create(1800,0);
+        this.groundSprite = game.add.sprite(0,350,'ground');
+		this.groundSprite.body.collideWorldBounds = false;
+		this.groundSprite.body.immovable = true;
         this.previousTime = (this.game.time.time / 100);
         this.hooks[this.numberOfHooks] = new Hook(this.game,this);
-        this.hooks[this.numberOfHooks].create(300,200);
+        this.hooks[this.numberOfHooks].create(300,100);
         this.numberOfHooks++;
-        this.hooks[this.numberOfHooks] = new Hook(this.game,this);
-        this.hooks[this.numberOfHooks].create(1100,200);
+		this.hooks[this.numberOfHooks] = new Hook(this.game,this);
+        this.hooks[this.numberOfHooks].create(900,100);
         this.numberOfHooks++;
-        this.hooks[this.numberOfHooks] = new Hook(this.game,this);
-        this.hooks[this.numberOfHooks].create(1900,200);
+		this.hooks[this.numberOfHooks] = new Hook(this.game,this);
+        this.hooks[this.numberOfHooks].create(1500,100);
         this.numberOfHooks++;
+		this.scoreSprite = game.add.sprite(0,12,'score');
+		this.scoreLabelString = "0";
+		this.style = {font: "30px Arial",fill: "#dad6cb", align: "left"};
+        this.score = game.add.text(75,11,this.scoreLabelString,this.style);
+
     },
     update: function(){
+	    if(this.scoreLabelString != this.userScore){
+            this.scoreLabelString = this.userScore;
+            this.score.setText(this.scoreLabelString);
+        }
         this.elapsedTime = (this.game.time.time / 100) - this.previousTime;
         this.previousTime = (this.game.time.time / 100);
-        for(var x  = 0; x < this.numberOfHooks; x++){
-            this.hooks[x].update();
-        }
+		this.scale(this.boardScale);
+		for(var x  = 0; x < this.numberOfHooks; x++){
+			this.hooks[x].scale(this.boardScale);
+			this.hooks[x].update();
+		}
+		this.leftWall.scale(this.boardScale);
+		this.rightWall.scale(this.boardScale);
+		this.leftWall.update();
+		this.rightWall.update();
     },
     updateBlocks: function(){
-
+    	
     },
     move: function(){
 
-    }
+    },
+	scale: function(newScale){
+		this.groundSprite.scale.setTo(1,1/newScale);
+		this.groundSprite.y = 400 - (50/newScale);
+	}
 }
--- Hook.js
+++ Hook.js
@@ -1,27 +1,38 @@
 /**
- * Created by Zachary on 4/4/2014.
- */
-/**
  * Created by Zachary on 3/1/14.
  */
 Hook = function(game,board){
     this.game = game;
     this.board = board;
     this.sprite = null;
-    this.xPosition = 0;
-    this.yPosition = 0;
+    this.xScreenPosition = 0;
+    this.yScreenPosition = 0;
+	this.xWorldPosition;
+	this.yWorldPosition;
+	this.currentScale = 1;
 };
 
 Hook.prototype = {
     preload: function(){
-
+    	
     },
     create: function(x,y){
-        this.sprite = game.add.sprite(x,y,'hook');
-        this.xPosition = this.sprite.x;
-        this.yPosition = this.sprite.y;
+    	this.sprite = game.add.sprite(x,y,'hook');
+    	this.xWorldPosition = this.sprite.x;
+    	this.yWorldPosition = this.sprite.y;
     },
     update: function(){
-        this.sprite.x = 350 + this.xPosition - this.board.userPosition;
-    }
+    	//this.sprite.x = 350 + this.xWorldPosition - this.board.userPosition;
+		this.xScreenPosition = this.sprite.x;
+		this.yScreenPosition = this.sprite.y;
+    },
+	scale: function(newScale){
+		var distanceToCenter = this.xWorldPosition - this.board.userPosition;
+		var newDistanceToCenter = distanceToCenter / newScale;
+		var distanceToGround = 400 - this.yWorldPosition;
+		var newDistanceToGround = distanceToGround / newScale;
+		this.sprite.x = 350 + newDistanceToCenter;
+		this.sprite.y = 400 - newDistanceToGround;
+		this.sprite.scale.setTo(1/newScale,1/newScale);
+	}
 }
